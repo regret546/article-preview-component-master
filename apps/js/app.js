34686620808d674218946c73a6d075e255e221e0
regret546 <iregret546@gmail.com>
2024-02-03 00:52:17 +0800

    Finish javascript and initial overall design

--- apps/js/app.js
+++ apps/js/app.js
@@ -1,9 +1,10 @@
 const shareButton = document.querySelector("#share-btn");
 const shareContent = document.querySelector("#share-content");
 
-shareButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (shareContent.style.display === "none") {
+shareButton.addEventListener("click", function () {
+  const computedStyle = window.getComputedStyle(shareContent);
+
+  if (computedStyle.display === "none") {
     shareContent.style.display = "flex";
   } else {
     shareContent.style.display = "none";
